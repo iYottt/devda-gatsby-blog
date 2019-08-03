0da11e7d6c7df13953b2322a6bcb8360c364fe1e
Yotsakon <iyottt@gmail.com>
2019-08-03 23:40:10 +0700

    edit title

--- content/meta/config.js
+++ content/meta/config.js
@@ -1,18 +1,18 @@
 module.exports = {
-  siteTitle: "HeroBlog - a GatsbyJS starter", // <title>
-  shortSiteTitle: "HeroBlog - GatsbyJS starter", // <title> ending for posts and pages
-  siteDescription: "HeroBlog is a GatsbyJS starter.",
+  siteTitle: "Devda - Web & Mobile Developer", // <title>
+  shortSiteTitle: "Devda - Web & Mobile Developer", // <title> ending for posts and pages
+  siteDescription: "Devda - Web & Mobile Developer",
   siteUrl: "https://gatsby-starter-hero-blog.greglobinski.com",
   // pathPrefix: "",
   siteImage: "preview.jpg",
   siteLanguage: "en",
 
   /* author */
-  authorName: "greg lobinski",
+  authorName: "Yotsakon Pitinanon",
   authorTwitterAccount: "greglobinski",
 
   /* info */
-  headerTitle: "greg lobinski",
+  headerTitle: "Yotsakon Pitinanon",
   headerSubTitle: "presents another one GatsbyJS starter",
 
   /* manifest.json */
@@ -32,8 +32,8 @@ module.exports = {
 
   // social
   authorSocialLinks: [
-    { name: "github", url: "https://github.com/greglobinski" },
-    { name: "twitter", url: "https://twitter.com/greglobinski" },
-    { name: "facebook", url: "http://facebook.com/greglobinski" }
+    { name: "github", url: "https://github.com/iyottt" },
+    { name: "twitter", url: "https://twitter.com/iyottt" },
+    { name: "facebook", url: "http://facebook.com/iyottt" }
   ]
 };
--- content/pages/1--about/index.md
+++ content/pages/1--about/index.md
@@ -2,7 +2,7 @@
 title: About
 ---
 
-This is a demo site of the [HeroBlog](https://github.com/greglobinski/gatsby-starter-hero-blog), a [GatsbyJS](https://www.gatsbyjs.org/) starter.
+This is a demo site of the [Devda](https://github.com/greglobinski/gatsby-starter-hero-blog), a [GatsbyJS](https://www.gatsbyjs.org/) starter.
 
 ### Features:
 
